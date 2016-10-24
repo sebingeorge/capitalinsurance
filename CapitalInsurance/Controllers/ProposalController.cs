11b1d6e028cb50d5268d76626654b06c4197c014
Jinu <jinu@abc.com>
2016-10-24 16:50:32 +0530

    layout

---
+++ CapitalInsurance/Controllers/ProposalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapitalInsurance.Controllers
+{
+    public class ProposalController : Controller
+    {
+        // GET: Proposal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateProposal()
+        {
+            return View();
+        }
+    }
+}
