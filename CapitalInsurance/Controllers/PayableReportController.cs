042a79dd6271cc260a1ec4a9ffd96e7751f769f8
Jinu <jinu@abc.com>
2016-11-02 16:44:38 +0530

    controller

---
+++ CapitalInsurance/Controllers/PayableReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapitalInsurance.Controllers
+{
+    public class PayableReportController : Controller
+    {
+        // GET: PayableReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Payment()
+        {
+            return View();
+        }
+        public ActionResult PayableReport()
+        {
+            return View();
+        }
+        public ActionResult PreviousPayable()
+        {
+            return View();
+        }
+    }
+}
