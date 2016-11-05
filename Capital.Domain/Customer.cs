c914a88b67dda1a15cd47860c7b2c9d299b418ab
Sreekanth A <sreekanth@ihitsindia.com>
2016-11-05 10:13:27 +0530

    Customer controller and Model

--- Capital.Domain/Customer.cs
+++ Capital.Domain/Customer.cs
@@ -8,10 +8,23 @@ namespace Capital.Domain
         {
            
         }
-
         public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        
+        public string CusName { get; set; }
+        public string CusShortName { get; set; }
+        public int RegionId { get; set; }
+        public int SalesMgId { get; set; }
+        public string IsProspectiveClient { get; set; }
+        public string ContactName { get; set; }
+        public string Designation { get; set; }
+        public string OfficeNo { get; set; }
+        public string MobileNo { get; set; }
+        public string EmailId { get; set; }
+        public string StateId { get; set; }
+        public int CountryId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public int CreditPeriod { get; set; }
+        public decimal CreditAmount { get; set; }
     }
 }
