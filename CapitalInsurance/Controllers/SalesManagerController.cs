858d0b94f1142a328538dafe453bcb1ff3a7b158
Sreekanth A <sreekanth@ihitsindia.com>
2016-11-08 14:13:02 +0530

    sales manager

---
+++ CapitalInsurance/Controllers/SalesManagerController.cs
@@ -0,0 +1,22 @@
+﻿using Capital.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapitalInsurance.Controllers
+{
+    public class SalesManagerController : Controller
+    {
+        // GET: SalesManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View(new SalesManager());
+        }
+    }
+}
