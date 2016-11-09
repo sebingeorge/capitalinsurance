a48dee5b02e830ddc7e170d3776c15d6eb152ade
Sreekanth A <sreekanth@ihitsindia.com>
2016-11-09 10:27:31 +0530

    Insurance Company

---
+++ CapitalInsurance/Controllers/InsuranceCompanyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Capital.Domain;
+
+namespace CapitalInsurance.Controllers
+{
+    public class InsuranceCompanyController : Controller
+    {
+        // GET: InsuranceCompany
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View(new InsuranceCompany());
+        }
+        [HttpPost]
+        public ActionResult Create(InsuranceCompany model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return View(model);
+            }
+            return RedirectToAction("Create");
+        }
+    }
+}
