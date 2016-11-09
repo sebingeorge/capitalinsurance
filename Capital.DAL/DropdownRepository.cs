ed951534c6b68a63cd290048b11b8e08f942a9b9
Midhu George <midhugeorge@gmail.com>
2016-11-09 17:26:12 +0530

    sales manager combo fill

---
+++ Capital.DAL/DropdownRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capital.Domain;
+using System.Data;
+using Dapper;
+
+namespace Capital.DAL
+{
+    public class DropdownRepository : BaseRepository
+    {
+        static string dataConnection = GetConnectionString("CibConnection");
+
+        public List<Dropdown> GetDesignation()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                return connection.Query<Dropdown>("SELECT DsgId Id, DsgName Name FROM Designation").ToList();
+            }
+        }
+
+      
+    }
+}
--- CapitalInsurance/Controllers/SalesManagerController.cs
+++ CapitalInsurance/Controllers/SalesManagerController.cs
@@ -1,4 +1,5 @@
-﻿using Capital.Domain;
+﻿using Capital.DAL;
+using Capital.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,17 +17,45 @@ namespace CapitalInsurance.Controllers
         }
         public ActionResult Create()
         {
+            FillDropdowns();
             return View(new SalesManager());
+        }
+        void FillDropdowns()
+        {
+            FillDesignation();
+            FillState();
+            FillCountry();
         }
         [HttpPost]
         public ActionResult Create(SalesManager model)
         {
+
             if (!ModelState.IsValid)
             {
                 var allErrors = ModelState.Values.SelectMany(v => v.Errors);
                 return View(model);
             }
+            Result res = new SalesManagerRepository().Insert(model);
             return RedirectToAction("Create");
+        }
+         void FillDesignation()
+        {
+
+            ViewBag.Designation = new SelectList((new DropdownRepository()).GetDesignation(), "Id", "Name");
+          
+        }
+        void FillState()
+        {
+            ViewBag.State = new SelectList((new StateRepository()).GetState(), "StateId", "StateName");
+        }
+        void FillCountry()
+        {
+            ViewBag.Country = new SelectList((new CountryRepository()).GetCountry(), "CountryId", "CountryName");
+        }
+        public ActionResult DepartmentPopup()
+        {
+            var List = new SalesManagerRepository().FillDepartmentList();
+            return View(List);
         }
     }
 }
