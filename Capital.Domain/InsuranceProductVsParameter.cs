573e90d970100a011cb4b8b4ce6b73fff9bf76af
Sreekanth A <sreekanth@ihitsindia.com>
2016-11-09 13:23:41 +0530

    Insurance Product Domain

---
+++ Capital.Domain/InsuranceProductVsParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+    public class InsuranceProductVsParameter
+    {
+        public int? InsParamRowId { get; set; }
+        public int? InsPrdId { get; set; }
+        public int? InsPrdParamId { get; set; }
+        public string InsParamValue { get; set; }
+        public string InsPrdParamName { get; set; }
+    }
+}
