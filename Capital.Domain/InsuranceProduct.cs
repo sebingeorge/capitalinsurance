7f458aa213296fc0de7a10a998d618f45b54b633
Sreekanth A <sreekanth@ihitsindia.com>
2016-11-09 13:15:10 +0530

    insurance product changes

---
+++ Capital.Domain/InsuranceProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+    public class InsuranceProduct
+    {
+        public int? InsPrdId { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Product Name is Required")]
+        public string InsPrdName { get; set; }
+        public string InsPrdShortName { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Product Type is Required")]
+        public int InsTypeId { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Product Company is Required")]
+        public int InsCmpId { get; set; }
+    }
+}
---
+++ Capital.Domain/InsuranceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+    public class InsuranceType
+    {
+        public int? InsTypeId { get; set; }
+        public string insTypeName { get; set; }
+    }
+}
