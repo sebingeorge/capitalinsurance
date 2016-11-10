ae2c6feae2868520c5866c0ae7ee033cda60d485
Midhu George <midhugeorge@gmail.com>
2016-11-10 12:32:30 +0530

    InsuranceCompany Save

---
+++ Capital.Domain/DropDown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+    public class Dropdown
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- CapitalInsurance/Controllers/InsuranceCompanyController.cs
+++ CapitalInsurance/Controllers/InsuranceCompanyController.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Capital.Domain;
+using Capital.DAL;
 
 namespace CapitalInsurance.Controllers
 {
-    public class InsuranceCompanyController : Controller
+    public class InsuranceCompanyController : BaseController
     {
         // GET: InsuranceCompany
         public ActionResult Index()
@@ -26,7 +27,7 @@ namespace CapitalInsurance.Controllers
                 var allErrors = ModelState.Values.SelectMany(v => v.Errors);
                 return View(model);
             }
-            //Result res = new SalesManagerRepository().Insert(model);
+            Result res = new InsuranceCompanyRepository().Insert(model);
             return RedirectToAction("Create");
         }
     }
