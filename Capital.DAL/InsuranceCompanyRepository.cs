24d990addecc14fd516b334c09c967cb88b726e1
Midhu George <midhugeorge@gmail.com>
2016-11-11 15:13:49 +0530

    Insurance Company Edit

--- Capital.DAL/InsuranceCompanyRepository.cs
+++ Capital.DAL/InsuranceCompanyRepository.cs
@@ -50,5 +50,83 @@ namespace Capital.DAL
             }
             return res;
         }
+
+        public List<InsuranceCompany> GetCompany()
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string query = @"select * from InsuranceCompany
+                order by InsCmpName";
+                return connection.Query<InsuranceCompany>(query).ToList();
+            }
+        }
+        public InsuranceCompany GetCompany(int Id)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string sql = @"select * from InsuranceCompany where InsCmpId=@Id";
+
+
+                var objCompany = connection.Query<InsuranceCompany>(sql, new
+                {
+                    Id = Id
+                }).First<InsuranceCompany>();
+
+                return objCompany;
+            }
+
+
+        }
+        public Result Update(InsuranceCompany model)
+        {
+            Result res = new Result(false);
+            try
+            {
+                using (IDbConnection connection = OpenConnection(dataConnection))
+                {
+                    string sql = @" UPDATE InsuranceCompany SET 
+                                    InsCmpName=@InsCmpName
+                                   ,Address1=@Address1
+                                   ,Address2=@Address2
+                                   ,Address3=@Address3
+                                   ,Email=@Email
+                                   ,ContactPerson=@ContactPerson
+                                   ,ContactNo=@ContactNo
+                                   ,Designation=@Designation
+                                   WHERE InsCmpId=@InsCmpId";
+                    int id = connection.Execute(sql, model);
+                    if (id > 0)
+                    {
+                        return (new Result(true));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return (new Result(false, ex.InnerException == null ? ex.Message : ex.InnerException.Message));
+            }
+            return res;
+        }
+        public Result Delete(InsuranceCompany model)
+        {
+            Result res = new Result(false);
+            try
+            {
+                using (IDbConnection connection = OpenConnection(dataConnection))
+                {
+                    string sql = @" Delete from InsuranceCompany WHERE InsCmpId=@InsCmpId";
+                    int id = connection.Execute(sql, model);
+                    if (id > 0)
+                    {
+                        return (new Result(true));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return (new Result(false, ex.InnerException == null ? ex.Message : ex.InnerException.Message));
+            }
+            return res;
+        }
     }
 }
--- CapitalInsurance/Controllers/InsuranceCompanyController.cs
+++ CapitalInsurance/Controllers/InsuranceCompanyController.cs
@@ -13,7 +13,8 @@ namespace CapitalInsurance.Controllers
         // GET: InsuranceCompany
         public ActionResult Index()
         {
-            return View();
+            List<InsuranceCompany> lstInsurCmpy = (new InsuranceCompanyRepository()).GetCompany();
+            return View(lstInsurCmpy);
         }
         public ActionResult Create()
         {
@@ -29,6 +30,47 @@ namespace CapitalInsurance.Controllers
             }
             Result res = new InsuranceCompanyRepository().Insert(model);
             return RedirectToAction("Create");
+        }
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit";
+            InsuranceCompany objCompany = new InsuranceCompanyRepository().GetCompany(Id);
+            return View("Create", objCompany);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(InsuranceCompany model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return View(model);
+            }
+            Result res = new InsuranceCompanyRepository().Update(model);
+
+
+            if (res.Value)
+            {
+                TempData["Success"] = "Updated Successfully!";
+            }
+            else
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            ViewBag.Title = "Delete";
+            InsuranceCompany objCompany = new InsuranceCompanyRepository().GetCompany(Id);
+            return View("Create", objCompany);
+        }
+        [HttpPost]
+        public ActionResult Delete(InsuranceCompany model)
+        {
+            Result res = new InsuranceCompanyRepository().Delete(model);
+            return RedirectToAction("Index");
         }
     }
 }
