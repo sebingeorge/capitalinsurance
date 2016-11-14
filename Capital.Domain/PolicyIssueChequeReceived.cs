dd149e798ca9c29b06bf4f721eec90d400dabc7d
Sreekanth A <sreekanth@ihitsindia.com>
2016-11-14 12:00:16 +0530

    policy issue

---
+++ Capital.Domain/PolicyIssueChequeReceived.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+    public class PolicyIssueChequeReceived
+    {
+        public int? InsChqRowId { get; set; }
+        public int? PolicyId { get; set; }
+        public string ChequeNo { get; set; }
+        public DateTime ChequeDate { get; set; }
+        public string BankName { get; set; }
+        public string BankBranch { get; set; }
+    }
+}
