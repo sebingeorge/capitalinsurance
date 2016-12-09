b7a0f2ce8b089a49503e4183a01e439f9be663b4
midhu <midhugeorge>
2016-12-09 15:35:58 +0530

    ageingreport

--- Capital.DAL/ReportRepository.cs
+++ Capital.DAL/ReportRepository.cs
@@ -21,6 +21,10 @@ namespace Capital.DAL
                      SELECT P.PolicyId,C.CusName,P.TranType,P.TotalPremium,0 Amount1,0 Amount2,0 Amount3,0 Amount4,0 Amount5 INTO #Result FROM PolicyIssue P inner join Customer C ON C.CusId=P.CusId where P.PolicyNo IS NOT NULL;
                      
                      with A as (
+                     select PolicyId, sum(ChequeAmt)Amount from PolicyIssueChequeReceived  group by PolicyId)
+                     update R set TotalPremium =(TotalPremium- A.Amount) from A inner join #Result R on R.PolicyId = A.PolicyId;
+
+                     with A as (
                      select PolicyId, sum(CommittedAmt)Amount from PolicyIssueCommittedDetails WHERE  DATEDIFF(DAY, GETDATE(), CommittedDate) <=15  group by PolicyId)
                      update R set R.Amount1 = A.Amount from A inner join #Result R on R.PolicyId = A.PolicyId;
                     
