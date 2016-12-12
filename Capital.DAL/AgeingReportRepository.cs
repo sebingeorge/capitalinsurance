9d8d6c646aa0108168b8069b75a617e15f30bed3
Sebin George <Sebin@ihits.com>
2016-12-12 12:10:54 +0530

    Aging in months

--- Capital.DAL/AgeingReportRepository.cs
+++ Capital.DAL/AgeingReportRepository.cs
@@ -17,19 +17,19 @@ namespace Capital.DAL
             using (IDbConnection connection = OpenConnection(dataConnection))
             {
                 string query = @"
-
-                    declare @LastDay_CurrentMonth datetime;
+declare @LastDay_CurrentMonth datetime;
 declare @LastDay_NextMonth datetime;
+declare @FirstDay_NextMonth datetime;
 declare @LastDay_Next2Month datetime;
-
+declare @FirstDay_Next2Month datetime;
 select @LastDay_CurrentMonth = (SELECT DATEADD(s,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE())+1,0)))
 
 select @LastDay_NextMonth =(SELECT DATEADD(s,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE())+2,0)))
-
-
+select @FirstDay_NextMonth =(SELECT DATEADD(s,1,@LastDay_CurrentMonth))
+select @FirstDay_Next2Month =(SELECT DATEADD(s,1,@LastDay_NextMonth))
 select @LastDay_Next2Month = (SELECT DATEADD(s,-1,DATEADD(mm, DATEDIFF(m,0,GETDATE())+3,0)))
 
-select @LastDay_CurrentMonth ,@LastDay_NextMonth,@LastDay_Next2Month
+select @LastDay_CurrentMonth ,@FirstDay_NextMonth,@LastDay_NextMonth,@FirstDay_Next2Month,@LastDay_Next2Month
 
 
  SELECT P.PolicyId,C.CusName,P.TranType,P.TotalPremium,0 Amount1,0 Amount2,0 Amount3 INTO #Result FROM PolicyIssue P inner join Customer C ON C.CusId=P.CusId where P.PolicyNo IS NOT NULL;
@@ -43,18 +43,12 @@ select @LastDay_CurrentMonth ,@LastDay_NextMonth,@LastDay_Next2Month
                      update R set R.Amount1 = A.Amount from A inner join #Result R on R.PolicyId = A.PolicyId;
                     
                      with A as (
-                     select PolicyId, sum(CommittedAmt)Amount from PolicyIssueCommittedDetails WHERE   CommittedDate BETWEEN @LastDay_CurrentMonth AND @LastDay_NextMonth  group by PolicyId)
+                     select PolicyId, sum(CommittedAmt)Amount from PolicyIssueCommittedDetails WHERE   CommittedDate BETWEEN @FirstDay_NextMonth AND @LastDay_NextMonth  group by PolicyId)
                      update R set R.Amount2 = A.Amount from A inner join #Result R on R.PolicyId = A.PolicyId;
                      
                      with A as (
-                     select PolicyId, sum(CommittedAmt)Amount from PolicyIssueCommittedDetails WHERE  CommittedDate BETWEEN @LastDay_NextMonth AND @LastDay_Next2Month  group by PolicyId)
+                     select PolicyId, sum(CommittedAmt)Amount from PolicyIssueCommittedDetails WHERE  CommittedDate BETWEEN @FirstDay_Next2Month AND @LastDay_Next2Month  group by PolicyId)
                      update R set R.Amount3 = A.Amount from A inner join #Result R on R.PolicyId = A.PolicyId;
-                     
-
-       
-                     
-
-					 
 
 
 				     select * from #Result where  CusName LIKE '%'+@Client+'%';
