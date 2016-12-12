cc41ff7a7fda7d8d20746b621470c8eda0e80585
midhu <midhugeorge>
2016-12-12 16:51:27 +0530

    renewalsummary

---
+++ CapitalInsurance/Controllers/PolicyRenewalSummaryReportController.cs
@@ -0,0 +1,30 @@
+﻿using Capital.DAL;
+using Capital.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapitalInsurance.Controllers
+{
+    public class PolicyRenewalSummaryReportController : BaseController
+    {
+        // GET: PolicyRenewalSummaryReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PolicyRenewalSummary()
+        {
+            return PartialView("_PolicyRenewalSummary", new ReportRepository().GetPolicyRenewalSummary());
+        }
+        public ActionResult PolicyDetailsPopup(int id = 0)
+        {
+            if (id != 0)
+                return PartialView("_PolicyDetailsPopup", new ReportRepository().GetPolicyDetails(id));
+            return View();
+        }
+
+    }
+}
