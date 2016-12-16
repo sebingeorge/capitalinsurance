3af8651a25695dcdf7a60bc73352d95c7796f21e
midhu <midhugeorge>
2016-12-16 15:45:02 +0530

    alert count

---
+++ Capital.Domain/QuickView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+  public class QuickView
+    {
+        public bool NewPolicy { get; set; }
+        public int NoOfNewPolicy { get; set; }
+        public bool RenewalPolicy { get; set; }
+        public int NoOfRenewalPolicy { get; set; }
+        public bool EndorsePolicy { get; set; }
+        public int NoOfEndorsePolicy { get; set; }
+        public bool SalesTarget { get; set; }
+        public int NoOfSalesTarget { get; set; }
+        public bool DailyActivity { get; set; }
+        public int NoOfDailyActivity { get; set; }
+
+    }
+}
--- CapitalInsurance/Controllers/HomeController.cs
+++ CapitalInsurance/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capital.DAL;
+using Capital.Domain;
+
 
 namespace CapitalInsurance.Controllers
 {
@@ -25,6 +28,49 @@ namespace CapitalInsurance.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [AllowAnonymous]
+        public ActionResult LoadQuickView()
+        {
+            QuickView view = new QuickView();
+            view.NewPolicy = true;
+            view.RenewalPolicy = true;
+            view.EndorsePolicy = true;
+            view.SalesTarget = true;
+
+            if (view.NewPolicy)
+            {
+                DateTime? FromDate=null;
+                DateTime? ToDate = null;
+                var res = new PolicyIssueRepository().GetNewPolicy(FromDate ?? FYStartdate, ToDate ?? DateTime.Now, "", "", "");
+                view.NoOfNewPolicy = res.Count;
+            }
+            if (view.RenewalPolicy)
+            {
+                DateTime FromDate = new PolicyRenewalRepository().GetFromDate(); ;
+                DateTime ToDate = new PolicyRenewalRepository().GetToDate();
+                var res = new PolicyRenewalRepository().GetNewPolicyForRenewal(FromDate,ToDate , "", "", "");
+                view.NoOfRenewalPolicy = res.Count;
+            }
+            if (view.EndorsePolicy)
+            {
+                DateTime? FromDate = null;
+                DateTime? ToDate = null;
+                var res = new PolicyEndorsementRepository().GetNewPolicyForEndorse(FromDate ?? FYStartdate, ToDate ?? DateTime.Now, "", "", "");
+                view.NoOfEndorsePolicy = res.Count;
+            }
+            if (view.DailyActivity)
+            {
+                var res = new DailyActivityRepository().GetDailyActivityDetails(0);
+                view.NoOfDailyActivity = res.Count;
+            }
+            if (view.SalesTarget)
+            {
+                var res = new SalesTargetRepository().GetEmployees();
+                view.NoOfSalesTarget = res.Count;
+            }
+           
+            return PartialView("_LoginPartial", view);
         }
     }
 }
