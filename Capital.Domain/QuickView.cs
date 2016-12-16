ea78c5a330f8974d380fb317948c5eaad7e11bf2
midhu <midhugeorge>
2016-12-16 16:20:06 +0530

    filter image removed

--- Capital.Domain/QuickView.cs
+++ Capital.Domain/QuickView.cs
@@ -8,16 +8,10 @@ namespace Capital.Domain
 {
   public class QuickView
     {
-        public bool NewPolicy { get; set; }
-        public int NoOfNewPolicy { get; set; }
+       
         public bool RenewalPolicy { get; set; }
         public int NoOfRenewalPolicy { get; set; }
-        public bool EndorsePolicy { get; set; }
-        public int NoOfEndorsePolicy { get; set; }
-        public bool SalesTarget { get; set; }
-        public int NoOfSalesTarget { get; set; }
-        public bool DailyActivity { get; set; }
-        public int NoOfDailyActivity { get; set; }
+       
 
     }
 }
--- CapitalInsurance/Controllers/HomeController.cs
+++ CapitalInsurance/Controllers/HomeController.cs
@@ -33,18 +33,9 @@ namespace CapitalInsurance.Controllers
         public ActionResult LoadQuickView()
         {
             QuickView view = new QuickView();
-            view.NewPolicy = true;
+          
             view.RenewalPolicy = true;
-            view.EndorsePolicy = true;
-            view.SalesTarget = true;
 
-            if (view.NewPolicy)
-            {
-                DateTime? FromDate=null;
-                DateTime? ToDate = null;
-                var res = new PolicyIssueRepository().GetNewPolicy(FromDate ?? FYStartdate, ToDate ?? DateTime.Now, "", "", "");
-                view.NoOfNewPolicy = res.Count;
-            }
             if (view.RenewalPolicy)
             {
                 DateTime FromDate = new PolicyRenewalRepository().GetFromDate(); ;
@@ -52,24 +43,7 @@ namespace CapitalInsurance.Controllers
                 var res = new PolicyRenewalRepository().GetNewPolicyForRenewal(FromDate,ToDate , "", "", "");
                 view.NoOfRenewalPolicy = res.Count;
             }
-            if (view.EndorsePolicy)
-            {
-                DateTime? FromDate = null;
-                DateTime? ToDate = null;
-                var res = new PolicyEndorsementRepository().GetNewPolicyForEndorse(FromDate ?? FYStartdate, ToDate ?? DateTime.Now, "", "", "");
-                view.NoOfEndorsePolicy = res.Count;
-            }
-            if (view.DailyActivity)
-            {
-                var res = new DailyActivityRepository().GetDailyActivityDetails(0);
-                view.NoOfDailyActivity = res.Count;
-            }
-            if (view.SalesTarget)
-            {
-                var res = new SalesTargetRepository().GetEmployees();
-                view.NoOfSalesTarget = res.Count;
-            }
-           
+          
             return PartialView("_LoginPartial", view);
         }
     }
