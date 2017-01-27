f387e6e44fdcb254a51e20cb76f8c8fa29f0b1c4
Midhu George <midhugeorge@gmail.com>
2017-01-27 14:15:51 +0530

    daily activity previous

--- Capital.DAL/DailyActivityRepository.cs
+++ Capital.DAL/DailyActivityRepository.cs
@@ -65,6 +65,64 @@ namespace Capital.DAL
                 return connection.Query<DailyActivityItem>(query, new { Id = Id, From = From, To = To, type = type }).ToList();
             }
         }
+        public List<DailyActivity> GetPreviousDailyActivityList(int id)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string query = @"select distinct Trandate,SalesMgId from DailyActivity where SalesMgId=@id";
+                return connection.Query<DailyActivity>(query, new { id = id }).ToList();
+            }
+        }
+        public List<DailyActivityItem> GetDailyActivity(int id)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string query = @"Select * from DailyActivity  where SalesMgId=@Id";
+                return connection.Query<DailyActivityItem>(query, new { id = id }).ToList();
+            }
+        }
+        public DailyActivity GetDailyActivityHdDetails(int Id)
+        {
+            using (IDbConnection connection = OpenConnection(dataConnection))
+            {
+                string query = "Select TranDate,DailyActivityDate,SalesMgName,D.SalesMgId,DsgName from DailyActivity D INNER JOIN SalesManager S ON S.SalesMgId=D.SalesMgId INNER JOIN Designation DS ON S.DsgId=DS.DsgId WHERE D.SalesMgId=@Id";
+                return connection.Query<DailyActivity>(query, new { Id = Id }).First<DailyActivity>();
+            }
+        }
+        public Result Update(DailyActivity model)
+        {
+            Result res = new Result(false);
+            try
+            {
+                  DateTime TranDate = model.TranDate;
+                  int SalesMgId = model.SalesMgId;
+                  int CreatedBy = model.CreatedBy;
+                  DateTime CreatedDate = DateTime.Now;
+                  using (IDbConnection connection = OpenConnection(dataConnection))
+                {
+                    string sql = @"DELETE FROM DailyActivity WHERE SalesMgId=@SalesMgId AND TranDate=@TranDate";
+                    connection.Execute(sql, model);
+                    foreach (var item in model.DailyActivityItems)
+                    {
+                        sql = @"INSERT INTO DailyActivity
+                                (TranDate,SalesMgId,DailyActivityDate,DailyActivityTime,DailyActivityCompany,DailyActivityContactNo,DailyActivityContactPerson,DailyActivityEmail,DailyActivityType,DailyActivityRemarks,CreatedBy,CreatedDate)
+                                 VALUES('" + TranDate + "'," + SalesMgId + ",@DailyActivityDate,@DailyActivityTime,@DailyActivityCompany,@DailyActivityContactNo,@DailyActivityContactPerson,@DailyActivityEmail,@DailyActivityType,@DailyActivityRemarks," + CreatedBy + ",'" + CreatedDate + "');  SELECT CAST(SCOPE_IDENTITY() as int);";
+
+                    }
+                    int id = connection.Execute(sql, model.DailyActivityItems);
+                    if (id > 0)
+                    {
+                        return (new Result(true));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return (new Result(false, ex.InnerException == null ? ex.Message : ex.InnerException.Message));
+            }
+            return res;
+        }
+
    }
 
    
