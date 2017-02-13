11daf7404794155fd5164c126235337b8d02ae19
Midhu George <midhugeorge@gmail.com>
2017-02-13 12:30:02 +0530

    sales incentive report modification

---
+++ Capital.Domain/SalesIncentive.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capital.Domain
+{
+ public  class SalesIncentive
+    {
+            
+      public int FyId { get; set; }
+      public int SalesMgId { get; set; }
+      public string SalesMgName { get; set; }
+      public string SalesMgCode { get; set; }
+      public decimal Benchmark { get; set; }
+      public int IncentivePerc { get; set; }
+      public int JanComm { get; set; }
+      public int JanInctve { get; set; }
+      public int FebComm { get; set; }
+       public int FebInctve { get; set; }
+      public int MarComm { get; set; }
+      public int MarInctve { get; set; }
+      public int AplComm { get; set; }
+      public int AplInctve { get; set; }
+      public int MayComm { get; set; }
+      public int MayInctve { get; set; }
+      public int JunComm { get; set; }
+      public int JunInctve { get; set; }
+      public int JulyComm { get; set; }
+      public int JulyInctve { get; set; }
+      public int AugComm { get; set; }
+      public int AugInctve { get; set; }
+      public int SepComm { get; set; }
+      public int SepInctve { get; set; }
+      public int OctComm { get; set; }
+      public int OctInctve { get; set; }
+      public int NovComm { get; set; }
+      public int NovInctve { get; set; }
+      public int DecComm { get; set; }
+      public int DecInctve { get; set; }
+     
+    
+   
+    }
+}
